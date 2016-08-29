c2d7eb3cdd850699177e2fb274d6b4dafc699820
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-08-29 18:51:20 -0500

    Add example curl statements

--- Sources/main.swift
+++ Sources/main.swift
@@ -27,10 +27,37 @@ public let kassandra = Kassandra()
 
 let id = "follow/:id"
 
+/**
+ Allows JSON to be consumed
+*/
 router.all("/*", middleware: BodyParser())
+
+/**
+ Gets all the tweets that I have subscribed to.
+ 
+ curl localhost:8090
+*/
 router.get("/", handler: getMyFeed)
+
+/**
+ Gets all the tweets that a particular user has written
+ 
+ curl localhost:8090/rfdickerson
+*/
 router.get("/:user", handler: getUserTweets)
+
+/**
+ Create a new tweet
+ 
+ curl -X POST -d "{\"tweet\": \"I just tweeted!\"}" localhost:8090 
+*/
 router.post("/", handler: tweet)
+
+/**
+ Follow a new author
+ 
+ curl -X PUT localhost:8090/rfdickerson
+*/
 router.put("/:user", handler: followAuthor)
 
 
